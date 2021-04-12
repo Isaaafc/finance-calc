83c1462fa0b669795ee891e648a7b9afc5ab8459
Isaaafc <isaaafc@gmail.com>
2021-04-12 16:09:42 +0800

    First commit without checking

---
+++ finance-calc/Program.fs
@@ -0,0 +1,22 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+/// <summary>
+/// Adds two doubles and returns the result.
+/// </summary>
+/// <returns>
+/// The sum of two doubles.
+/// </returns>
+/// and the other is greater than zero.</exception>
+/// <param name="a">A double precision number.</param>
+/// <param name="b">A double precision number.</param>
+let compositeSavings x n target growth = 
+    let composite i = 
+        
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
